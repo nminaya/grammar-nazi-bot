e8e6db553ea94e39e393a5eb3b56e3e5565e2b93
nminaya <nelsonminayaperez@gmail.com>
2020-08-12 20:29:11 -0400

    Add Telegram.Bot for enviroment testing

--- GrammarNazi.App/HostedServices/BotHostedService.cs
+++ GrammarNazi.App/HostedServices/BotHostedService.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace GrammarNazi.App.HostedServices
 {
@@ -9,15 +11,34 @@ namespace GrammarNazi.App.HostedServices
     {
         private readonly ILogger<BotHostedService> _logger;
 
+        // This is for test only. This will be moved to a library
+        private readonly TelegramBotClient _client;
+
         public BotHostedService(ILogger<BotHostedService> logger)
         {
             _logger = logger;
+
+            var apiKey = Environment.GetEnvironmentVariable("TELEGRAM_API_KEY");
+
+            if (string.IsNullOrEmpty(apiKey))
+                throw new Exception("Empty TELEGRAM_API_KEY");
+
+            _client = new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_API_KEY"));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation("Bot Hosted Service started");
+            _client.StartReceiving();
+            _client.OnMessage += Client_OnMessage;
+
+            _logger.LogWarning("Bot Hosted Service started");
             await Task.Delay(int.MaxValue, stoppingToken);
+        }
+
+        private void Client_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            _logger.LogError($"Message reveived: {e.Message.Text}");
+            _client.SendTextMessageAsync(e.Message.Chat.Id, "Message received");
         }
     }
 }
