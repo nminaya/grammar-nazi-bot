ac726533592b26bbdbd1dc412640078ab1c24eb2
Nelson <nelsonminayaperez@gmail.com>
2020-08-13 14:47:24 -0400

    Add result entities

---
+++ GrammarNazi.Domain/Entities/CheckResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarNazi.Domain.Entities
+{
+    public class CheckResult
+    {
+        public bool HasErrors { get; set; }
+
+        public IEnumerable<ResultError> ResultErrors { get; set; }
+    }
+}
---
+++ GrammarNazi.Domain/Entities/ResultError.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarNazi.Domain.Entities
+{
+    public class ResultError
+    {
+        public string WrongWord { get; set; }
+        public string CorrectWord { get; set; }
+    }
+}
