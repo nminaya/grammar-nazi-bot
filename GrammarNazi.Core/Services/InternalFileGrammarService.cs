1eeb102eaec731c1b748abdc7c96bc7f542c78bb
Nelson <nelsonminayaperez@gmail.com>
2020-08-14 08:39:34 -0400

    Add names library for internal grammar engine

--- GrammarNazi.Core/Services/InternalFileGrammarService.cs
+++ GrammarNazi.Core/Services/InternalFileGrammarService.cs
@@ -1,5 +1,6 @@
 ﻿using GrammarNazi.Domain.Entities;
 using GrammarNazi.Domain.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -20,24 +21,24 @@ namespace GrammarNazi.Core.Services
 
         public Task<CheckResult> GetCorrections(string text)
         {
-            const string dictionaryFilePath = "Library/words_en-US.txt";
-
-            var words = text.Split(" ").Select(v => v.ToLower());
-            var dictionary = _fileService.GetTextFileByLine(dictionaryFilePath);
+            var dictionary = _fileService.GetTextFileByLine("Library/words_en-US.txt");
+            var names = _fileService.GetTextFileByLine("Library/names.txt");
 
             var resultErros = new List<ResultError>();
+
+            var words = text.Split(" ");
 
             foreach (var item in words.Where(v => !v.StartsWith("/")))
             {
                 // Remove special characters
-                var word = Regex.Replace(item, "[^0-9a-zA-Z:,]+", "");
+                var word = Regex.Replace(item, "[^0-9a-zA-Z:,]+", "").ToLower();
 
-                var wordFound = dictionary.Any(v => v == word);
+                var wordFound = dictionary.Any(v => v == word) || names.Any(v => string.Equals(v, word, StringComparison.OrdinalIgnoreCase));
 
                 if (!wordFound)
                 {
                     //TODO: Add correct word
-                    resultErros.Add(new ResultError { CorrectWord = "", WrongWord = word });
+                    resultErros.Add(new ResultError { CorrectWord = "", WrongWord = item });
                 }
             }
 
