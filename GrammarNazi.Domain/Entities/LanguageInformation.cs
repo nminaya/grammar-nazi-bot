02ed55443cf053a3c8444ddb22911fcf67b40756
nminaya <nelsonminayaperez@gmail.com>
2020-08-16 18:13:07 -0400

    Add ILanguageService

---
+++ GrammarNazi.Domain/Entities/LanguageInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarNazi.Domain.Entities
+{
+    public class LanguageInformation
+    {
+        public string LanguageCode { get; set; }
+        public string Iso639 { get; }
+        public string EnglishName { get; }
+        public string LocalName { get; }
+    }
+}
---
+++ GrammarNazi.Domain/Services/ILanguageService.cs
@@ -0,0 +1,12 @@
+﻿using GrammarNazi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarNazi.Domain.Services
+{
+    public interface ILanguageService
+    {
+        LanguageInformation IdentifyLanguage(string text);
+    }
+}
