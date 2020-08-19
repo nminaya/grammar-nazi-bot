5d435a373431427601c007f04ff63aa10487df95
nminaya <nelsonminayaperez@gmail.com>
2020-08-18 20:21:20 -0400

    Add LanguageService class

---
+++ GrammarNazi.Core/Services/LanguageService.cs
@@ -0,0 +1,16 @@
+﻿using GrammarNazi.Domain.Entities;
+using GrammarNazi.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarNazi.Core.Services
+{
+    public class LanguageService : ILanguageService
+    {
+        public LanguageInformation IdentifyLanguage(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
