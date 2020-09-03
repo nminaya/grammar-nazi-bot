4b362676f05c99936691e6d92ec1c256da46939f
nminaya <nelsonminayaperez@gmail.com>
2020-09-03 15:50:32 -0400

    Add GrammarNaziContext

---
+++ GrammarNazi.Core/GrammarNaziContext.cs
@@ -0,0 +1,19 @@
+﻿using GrammarNazi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrammarNazi.Core
+{
+    public class GrammarNaziContext : DbContext
+    {
+        public GrammarNaziContext(DbContextOptions<GrammarNaziContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChatConfiguration>()
+                .HasKey(v => v.ChatId);
+        }
+    }
+}
