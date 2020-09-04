64d50b149fe45de0be6c827f15181f1ec08a1ff1
nminaya <nelsonminayaperez@gmail.com>
2020-09-03 20:16:03 -0400

    Create TwitterLog.cs

---
+++ GrammarNazi.Domain/Entities/TwitterLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarNazi.Domain.Entities
+{
+    public class TwitterLog
+    {
+        public long TweetId { get; set; }
+        public long ReplyTweetId { get; set; }
+    }
+}
