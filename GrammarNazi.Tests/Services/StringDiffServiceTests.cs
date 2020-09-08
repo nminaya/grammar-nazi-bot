8195c961d66715ac0408e3be033d4b1481ec9e02
nminaya <nelsonminayaperez@gmail.com>
2020-09-08 17:40:44 -0400

    Add StringDiffService Tests

---
+++ GrammarNazi.Tests/Services/StringDiffServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using GrammarNazi.Core.Services;
+using Xunit;
+
+namespace GrammarNazi.Tests.Services
+{
+    public class StringDiffServiceTests
+    {
+        [Theory]
+        [InlineData("TEST", "TEST", 0)]
+        [InlineData("TEST", "BEST", 1)]
+        [InlineData("TEST", "TETS", 2)]
+        [InlineData("TEST", "TTES", 2)]
+        [InlineData("TEST", "TEST1", 1)]
+        public void ComputeDistance_GivenTwoStrings_Should_ReturnExpectedResult(string a, string b, int expectedResult)
+        {
+            // Arrange
+            var service = new StringDiffService();
+
+            // Act
+            var result = service.ComputeDistance(a, b);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
