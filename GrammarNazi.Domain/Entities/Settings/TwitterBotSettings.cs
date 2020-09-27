199cfa12c8fcdbb77850c5dc5eccbaa7c96277e3
nminaya <nelsonminayaperez@gmail.com>
2020-09-26 23:08:15 -0400

    Create TwitterBotSettings.cs

---
+++ GrammarNazi.Domain/Entities/Settings/TwitterBotSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace GrammarNazi.Domain.Entities.Configs
+{
+    public class TwitterBotSettings
+    {
+        public string BotUsername { get; set; }
+        public int TimelineFirstLoadPageSize { get; set; }
+        public int PublishTweetDelayMilliseconds { get; set; }
+        public int HostedServiceIntervalMilliseconds { get; set; }
+    }
+}
