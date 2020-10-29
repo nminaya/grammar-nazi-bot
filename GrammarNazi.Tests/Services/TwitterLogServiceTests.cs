40529911e4db2623bd993cc880f52ca2652147ce
nminaya <nelsonminayaperez@gmail.com>
2020-10-28 22:11:56 -0400

    Add GetLastTweetId tests for TwitterLogService

---
+++ GrammarNazi.Tests/Services/TwitterLogServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using GrammarNazi.Core.Services;
+using GrammarNazi.Domain.Entities;
+using GrammarNazi.Domain.Repositories;
+using Moq;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GrammarNazi.Tests.Services
+{
+    public class TwitterLogServiceTests
+    {
+        [Fact]
+        public async Task GetLastTweetId_RepositoryEmpty_Should_ReturnsZero()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IRepository<TwitterLog>>();
+
+            // Returns false when Any is called
+            repositoryMock.Setup(v => v.Any(It.IsAny<Expression<Func<TwitterLog, bool>>>()))
+                .ReturnsAsync(false);
+
+            var service = new TwitterLogService(repositoryMock.Object);
+
+            // Act
+            var result = await service.GetLastTweetId();
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public async Task GetLastTweetId_RepositoryNotEmpty_Should_ReturnsMaxTweetId()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IRepository<TwitterLog>>();
+
+            const long maxTweetId = 123456;
+
+            // Returns true when Any is called
+            repositoryMock.Setup(v => v.Any(It.IsAny<Expression<Func<TwitterLog, bool>>>()))
+                .ReturnsAsync(true);
+
+            // Returns maxTweetId value when Max is called
+            repositoryMock.Setup(v => v.Max(It.IsAny<Expression<Func<TwitterLog, long>>>()))
+                .ReturnsAsync(maxTweetId);
+
+            var service = new TwitterLogService(repositoryMock.Object);
+
+            // Act
+            var result = await service.GetLastTweetId();
+
+            // Assert
+            Assert.Equal(maxTweetId, result);
+        }
+    }
+}
