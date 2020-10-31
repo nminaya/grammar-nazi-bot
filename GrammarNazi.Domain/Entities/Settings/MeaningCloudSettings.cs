ea2858e4ad7bcb971ee7709bf710d30ff73821c8
nminaya <nelsonminayaperez@gmail.com>
2020-10-30 22:21:02 -0400

    Add new settings

---
+++ GrammarNazi.Domain/Entities/Settings/MeaningCloudSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GrammarNazi.Domain.Entities.Settings
+{
+    public class MeaningCloudSettings
+    {
+        public string MeaningCloudHostUrl { get; set; }
+        public string Key { get; set; }
+    }
+}
