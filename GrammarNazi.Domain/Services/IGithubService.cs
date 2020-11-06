701499ff4253f39c7583b23a848ede6c2ae28749
nminaya <nelsonminayaperez@gmail.com>
2020-11-05 23:13:24 -0400

    Create IGithubService.cs

---
+++ GrammarNazi.Domain/Services/IGithubService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Domain.Services
+{
+    public interface IGithubService
+    {
+        Task CreateBugIssue(string title, Exception exception);
+    }
+}
