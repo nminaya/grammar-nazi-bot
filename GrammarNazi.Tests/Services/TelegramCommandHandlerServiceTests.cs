530d49cb44a1194600f3a3112f0359a47470dc57
nminaya <nelsonminayaperez@gmail.com>
2020-11-07 00:01:40 -0400

    Add Start_NotChatCongfig_Should_CreateChatConfigAndReplyWelcomeMessage Test

---
+++ GrammarNazi.Tests/Services/TelegramCommandHandlerServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using GrammarNazi.Core.Services;
+using GrammarNazi.Domain.Constants;
+using GrammarNazi.Domain.Entities;
+using GrammarNazi.Domain.Services;
+using Moq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Xunit;
+
+namespace GrammarNazi.Tests.Services
+{
+    public class TelegramCommandHandlerServiceTests
+    {
+        [Theory]
+        [InlineData("/start")]
+        [InlineData("/start@" + Defaults.TelegramBotUser)]
+        public async Task Start_NotChatCongfig_Should_CreateChatConfigAndReplyWelcomeMessage(string command)
+        {
+            // Arrange
+            var chatConfigurationServiceMock = new Mock<IChatConfigurationService>();
+            var telegramBotClientMock = new Mock<ITelegramBotClient>();
+            var service = new TelegramCommandHandlerService(chatConfigurationServiceMock.Object, telegramBotClientMock.Object);
+            const string welcomeMessage = "Hi, I'm GrammarNazi";
+
+            var message = new Message
+            {
+                Text = command,
+                Chat = new Chat
+                {
+                    Id = 1
+                }
+            };
+
+            chatConfigurationServiceMock.Setup(v => v.GetConfigurationByChatId(message.Chat.Id))
+                .ReturnsAsync((ChatConfiguration)null);
+
+            // Act
+            await service.HandleCommand(message);
+
+            // Assert
+            chatConfigurationServiceMock.Verify(v => v.AddConfiguration(It.IsAny<ChatConfiguration>()), Times.Once);
+            telegramBotClientMock.Verify(v => v.SendTextMessageAsync(It.IsAny<ChatId>(), It.Is<string>(s => s.Contains(welcomeMessage)), ParseMode.Default, false, false, 0, null, default));
+        }
+    }
+}
