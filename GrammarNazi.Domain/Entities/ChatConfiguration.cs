0eb1b4a36913a30c7727a55dddea220814285f93
nminaya <nelsonminayaperez@gmail.com>
2020-11-12 15:11:02 -0400

    Use CorrectionStrictnessLevels.Intolerant as default

--- GrammarNazi.Domain/Entities/ChatConfiguration.cs
+++ GrammarNazi.Domain/Entities/ChatConfiguration.cs
@@ -10,7 +10,7 @@ namespace GrammarNazi.Domain.Entities
 
         public SupportedLanguages SelectedLanguage { get; set; }
 
-        public CorrectionStrictnessLevels CorrectionStrictnessLevel { get; set; } = CorrectionStrictnessLevels.Tolerant;
+        public CorrectionStrictnessLevels CorrectionStrictnessLevel { get; set; } = CorrectionStrictnessLevels.Intolerant;
 
         public bool IsBotStopped { get; set; }
 
