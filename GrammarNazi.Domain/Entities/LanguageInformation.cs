08655c3e8541ae764ff9fddde13716d5e1b9820e
nminaya <nelsonminayaperez@gmail.com>
2020-11-13 08:45:46 -0400

    Add documentation for LanguageInformation
    Fix #8

--- GrammarNazi.Domain/Entities/LanguageInformation.cs
+++ GrammarNazi.Domain/Entities/LanguageInformation.cs
@@ -1,8 +1,18 @@
 ﻿namespace GrammarNazi.Domain.Entities
 {
+    /// <summary>
+    /// ISO 639 Code Representation for a Language
+    /// </summary>
     public class LanguageInformation
     {
+        /// <summary>
+        /// The ISO 639-2 two-letter code for the language
+        /// </summary>
         public string TwoLetterISOLanguageName { get; set; }
+
+        /// <summary>
+        /// The ISO 639-3 three-letter code for the language
+        /// </summary>
         public string ThreeLetterISOLanguageName { get; set; }
     }
 }
