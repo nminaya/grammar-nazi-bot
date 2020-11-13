6088595950c93766d31194c293081ca923b55067
nminaya <nelsonminayaperez@gmail.com>
2020-11-13 08:34:57 -0400

    Add documentation to ChatConfiguration

--- GrammarNazi.Domain/Entities/ChatConfiguration.cs
+++ GrammarNazi.Domain/Entities/ChatConfiguration.cs
@@ -2,18 +2,39 @@
 
 namespace GrammarNazi.Domain.Entities
 {
+    /// <summary>
+    /// Bot Configuration for a Chat
+    /// </summary>
     public class ChatConfiguration
     {
+        /// <summary>
+        /// Unique identifier for the Chat
+        /// </summary>
         public long ChatId { get; set; }
 
+        /// <summary>
+        /// Selected GrammarAlgorithm
+        /// </summary>
         public GrammarAlgorithms GrammarAlgorithm { get; set; }
 
+        /// <summary>
+        /// Selected SupportedLanguages
+        /// </summary>
         public SupportedLanguages SelectedLanguage { get; set; }
 
+        /// <summary>
+        /// Selected CorrectionStrictnessLevels
+        /// </summary>
         public CorrectionStrictnessLevels CorrectionStrictnessLevel { get; set; } = CorrectionStrictnessLevels.Intolerant;
 
+        /// <summary>
+        /// True if bot stopped
+        /// </summary>
         public bool IsBotStopped { get; set; }
 
+        /// <summary>
+        /// True if correction details hidden
+        /// </summary>
         public bool HideCorrectionDetails { get; set; }
 
         public override bool Equals(object obj)
