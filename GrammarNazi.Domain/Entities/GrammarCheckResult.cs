a2474a04cfb628980a98157eeea97e86bdd560c7
nminaya <nelsonminayaperez@gmail.com>
2020-11-13 08:37:33 -0400

    Add documentation for GrammarCheckResult

--- GrammarNazi.Domain/Entities/GrammarCheckResult.cs
+++ GrammarNazi.Domain/Entities/GrammarCheckResult.cs
@@ -3,10 +3,19 @@ using System.Linq;
 
 namespace GrammarNazi.Domain.Entities
 {
+    /// <summary>
+    /// Represents the result of Grammar check
+    /// </summary>
     public class GrammarCheckResult
     {
+        /// <summary>
+        /// True if there is any Correction
+        /// </summary>
         public bool HasCorrections => Corrections?.Any() == true;
 
+        /// <summary>
+        /// List of Corrections
+        /// </summary>
         public IEnumerable<GrammarCorrection> Corrections { get; }
 
         public GrammarCheckResult(IEnumerable<GrammarCorrection> corrections)
