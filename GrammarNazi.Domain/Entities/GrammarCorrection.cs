e35f12d8275844a730806018fb76c70da833aaa0
nminaya <nelsonminayaperez@gmail.com>
2020-11-13 08:41:30 -0400

    Add documentation for GrammarCorrection

--- GrammarNazi.Domain/Entities/GrammarCorrection.cs
+++ GrammarNazi.Domain/Entities/GrammarCorrection.cs
@@ -2,10 +2,24 @@
 
 namespace GrammarNazi.Domain.Entities
 {
+    /// <summary>
+    /// Represents the grammar correction of a word or part of a text
+    /// </summary>
     public class GrammarCorrection
     {
+        /// <summary>
+        /// The grammatically wrong word
+        /// </summary>
         public string WrongWord { get; set; }
+
+        /// <summary>
+        /// Error message related to the wrong word
+        /// </summary>
         public string Message { get; set; }
+
+        /// <summary>
+        /// List of possible replacements for the wrong word
+        /// </summary>
         public IEnumerable<string> PossibleReplacements { get; set; }
     }
 }
