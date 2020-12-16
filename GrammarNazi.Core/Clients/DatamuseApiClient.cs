c86dc75852128383ba899da0aec57fc8a498aaa3
nminaya <nelsonminayaperez@gmail.com>
2020-12-15 21:33:47 -0400

    Create DatamuseApiClient

---
+++ GrammarNazi.Core/Clients/DatamuseApiClient.cs
@@ -0,0 +1,30 @@
+﻿using GrammarNazi.Domain.Clients;
+using GrammarNazi.Domain.Entities.DatamuseAPI;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GrammarNazi.Core.Clients
+{
+    public class DatamuseApiClient : IDatamuseApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DatamuseApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IEnumerable<WordCheck>> CheckWord(string word, string language)
+        {
+            var url = $"https://api.datamuse.com/words?={HttpUtility.UrlEncode(word)}&v={language}";
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(url);
+
+            return JsonConvert.DeserializeObject<IEnumerable<WordCheck>>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ GrammarNazi.Domain/Clients/IDatamuseApiClient.cs
@@ -0,0 +1,11 @@
+﻿using GrammarNazi.Domain.Entities.DatamuseAPI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Domain.Clients
+{
+    public interface IDatamuseApiClient
+    {
+        Task<IEnumerable<WordCheck>> CheckWord(string word, string language);
+    }
+}
