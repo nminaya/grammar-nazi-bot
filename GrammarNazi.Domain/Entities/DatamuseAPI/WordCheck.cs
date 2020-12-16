7e5824993c679ee0e6aba6acd8952afefff49f40
nminaya <nelsonminayaperez@gmail.com>
2020-12-15 22:06:18 -0400

    Update WordCheck.cs

--- GrammarNazi.Domain/Entities/DatamuseAPI/WordCheck.cs
+++ GrammarNazi.Domain/Entities/DatamuseAPI/WordCheck.cs
@@ -1,8 +1,13 @@
-﻿namespace GrammarNazi.Domain.Entities.DatamuseAPI
+﻿using Newtonsoft.Json;
+
+namespace GrammarNazi.Domain.Entities.DatamuseAPI
 {
     public class WordCheck
     {
+        [JsonProperty("word")]
         public string Word { get; set; }
+
+        [JsonProperty("score")]
         public int Score { get; set; }
     }
 }
