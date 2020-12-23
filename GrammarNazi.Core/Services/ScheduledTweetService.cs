2f4c481932642dfa8789bf974d44187dc9f8cf6f
nminaya <nelsonminayaperez@gmail.com>
2020-12-22 22:07:21 -0400

    Create ScheduledTweetService

---
+++ GrammarNazi.Core/Services/ScheduledTweetService.cs
@@ -0,0 +1,34 @@
+﻿using GrammarNazi.Domain.Entities;
+using GrammarNazi.Domain.Repositories;
+using GrammarNazi.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Core.Services
+{
+	public class ScheduledTweetService : IScheduledTweetService
+	{
+		private readonly IRepository<ScheduledTweet> _repository;
+
+		public ScheduledTweetService(IRepository<ScheduledTweet> repository)
+		{
+			_repository = repository;
+		}
+
+		public Task Add(ScheduledTweet scheduledTweet)
+		{
+			return _repository.Add(scheduledTweet);
+		}
+
+		public Task<IEnumerable<ScheduledTweet>> GetPendingScheduledTweets()
+		{
+			return _repository.GetAll(v => v.PublishAfter < DateTime.Now);
+		}
+
+		public Task Update(ScheduledTweet scheduledTweet)
+		{
+			return _repository.Update(scheduledTweet, v => v.Id == scheduledTweet.Id);
+		}
+	}
+}
---
+++ GrammarNazi.Domain/Services/IScheduledTweetService.cs
@@ -0,0 +1,15 @@
+﻿using GrammarNazi.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Domain.Services
+{
+	public interface IScheduledTweetService
+	{
+		Task<IEnumerable<ScheduledTweet>> GetPendingScheduledTweets();
+
+		Task Update(ScheduledTweet scheduledTweet);
+
+		Task Add(ScheduledTweet scheduledTweet);
+	}
+}
