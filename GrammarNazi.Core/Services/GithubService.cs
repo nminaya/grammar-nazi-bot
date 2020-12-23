47be30ce41cb542465d82c92e34961a06661a414
nminaya <nelsonminayaperez@gmail.com>
2020-12-23 17:28:20 -0400

    Update GithubService.cs

--- GrammarNazi.Core/Services/GithubService.cs
+++ GrammarNazi.Core/Services/GithubService.cs
@@ -9,52 +9,52 @@ using System.Threading.Tasks;
 
 namespace GrammarNazi.Core.Services
 {
-	public class GithubService : IGithubService
-	{
-		private readonly IGitHubClient _githubClient;
-		private readonly GithubSettings _githubSettings;
+    public class GithubService : IGithubService
+    {
+        private readonly IGitHubClient _githubClient;
+        private readonly GithubSettings _githubSettings;
 
-		public GithubService(IGitHubClient githubClient, IOptions<GithubSettings> options)
-		{
-			_githubClient = githubClient;
-			_githubSettings = options.Value;
-		}
+        public GithubService(IGitHubClient githubClient, IOptions<GithubSettings> options)
+        {
+            _githubClient = githubClient;
+            _githubSettings = options.Value;
+        }
 
-		public async Task CreateBugIssue(string title, Exception exception)
-		{
-			var bodyBuilder = new StringBuilder();
+        public async Task CreateBugIssue(string title, Exception exception)
+        {
+            var bodyBuilder = new StringBuilder();
 
-			var issue = await GetIssueByTittle(title);
+            var issue = await GetIssueByTittle(title);
 
-			// Leave a comment in the Issue if Issue exist
-			if (issue != null)
-			{
-				bodyBuilder.Append("GramarNazi Bot: Same exception captured again.\n\n");
-				bodyBuilder.AppendLine($"Date (UTC): {DateTime.UtcNow}\n\n");
-				bodyBuilder.AppendLine("StackTrace:\n\n").AppendLine(exception.ToString());
+            // Leave a comment in the Issue if Issue exist
+            if (issue != null)
+            {
+                bodyBuilder.Append("GramarNazi Bot: Same exception captured again.\n\n");
+                bodyBuilder.AppendLine($"Date (UTC): {DateTime.UtcNow}\n\n");
+                bodyBuilder.AppendLine("StackTrace:\n\n").AppendLine(exception.ToString());
 
-				await _githubClient.Issue.Comment.Create(_githubSettings.Username, _githubSettings.RepositoryName, issue.Number, bodyBuilder.ToString());
-				return;
-			}
+                await _githubClient.Issue.Comment.Create(_githubSettings.Username, _githubSettings.RepositoryName, issue.Number, bodyBuilder.ToString());
+                return;
+            }
 
-			bodyBuilder.Append("This is an issue created automatically by GrammarNazi when an exception was captured.\n\n");
-			bodyBuilder.AppendLine($"Date (UTC): {DateTime.UtcNow}\n\n");
-			bodyBuilder.AppendLine("Exception:\n\n").AppendLine(exception.ToString());
+            bodyBuilder.Append("This is an issue created automatically by GrammarNazi when an exception was captured.\n\n");
+            bodyBuilder.AppendLine($"Date (UTC): {DateTime.UtcNow}\n\n");
+            bodyBuilder.AppendLine("Exception:\n\n").AppendLine(exception.ToString());
 
-			var newIssue = new NewIssue(title)
-			{
-				Body = bodyBuilder.ToString()
-			};
-			newIssue.Labels.Add("production-bug");
+            var newIssue = new NewIssue(title)
+            {
+                Body = bodyBuilder.ToString()
+            };
+            newIssue.Labels.Add("production-bug");
 
-			await _githubClient.Issue.Create(_githubSettings.Username, _githubSettings.RepositoryName, newIssue);
-		}
+            await _githubClient.Issue.Create(_githubSettings.Username, _githubSettings.RepositoryName, newIssue);
+        }
 
-		private async Task<Issue> GetIssueByTittle(string title)
-		{
-			var issues = await _githubClient.Issue.GetAllForRepository(_githubSettings.Username, _githubSettings.RepositoryName);
+        private async Task<Issue> GetIssueByTittle(string title)
+        {
+            var issues = await _githubClient.Issue.GetAllForRepository(_githubSettings.Username, _githubSettings.RepositoryName);
 
-			return issues.FirstOrDefault(v => v.Title == title);
-		}
-	}
+            return issues.FirstOrDefault(v => v.Title == title);
+        }
+    }
 }
