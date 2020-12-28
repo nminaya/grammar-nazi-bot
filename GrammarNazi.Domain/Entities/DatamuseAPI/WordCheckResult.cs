c50eee4d44bdc8c440d7cf57408fb9170221bfc7
nminaya <nelsonminayaperez@gmail.com>
2020-12-28 08:25:40 -0400

    Add documentation to some entities

--- GrammarNazi.Domain/Entities/DatamuseAPI/WordCheckResult.cs
+++ GrammarNazi.Domain/Entities/DatamuseAPI/WordCheckResult.cs
@@ -4,11 +4,24 @@ using System.Linq;
 
 namespace GrammarNazi.Domain.Entities.DatamuseAPI
 {
+    /// <summary>
+    /// Datamuse API Word Check
+    /// </summary>
     public class WordCheckResult
     {
+        /// <summary>
+        /// Word to Check
+        /// </summary>
         public string Word { get; set; }
+
+        /// <summary>
+        /// List of words that are spelled similarly to 'Word'
+        /// </summary>
         public IEnumerable<WordSimilarity> SimilarWords { get; set; }
 
+        /// <summary>
+        /// True if 'SimilarWord' doesn't contains 'Word'
+        /// </summary>
         public bool IsWrongWord => SimilarWords.Any() && SimilarWords.All(v => !v.Word.Equals(Word, StringComparison.OrdinalIgnoreCase));
     }
 }
--- GrammarNazi.Domain/Entities/DatamuseAPI/WordSimilarity.cs
+++ GrammarNazi.Domain/Entities/DatamuseAPI/WordSimilarity.cs
@@ -2,11 +2,20 @@
 
 namespace GrammarNazi.Domain.Entities.DatamuseAPI
 {
+    /// <summary>
+    /// Datamuse API Word Similarity
+    /// </summary>
     public class WordSimilarity
     {
+        /// <summary>
+        /// Word
+        /// </summary>
         [JsonProperty("word")]
         public string Word { get; set; }
 
+        /// <summary>
+        /// Similarity score
+        /// </summary>
         [JsonProperty("score")]
         public int Score { get; set; }
     }
