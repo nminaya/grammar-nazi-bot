569afbb38876ec7e5e279285c01da8c18f1059a3
nminaya <nelsonminayaperez@gmail.com>
2021-01-07 08:49:24 -0400

    Add NoLastTweetId_Should_UsePageSize test

--- GrammarNazi.Tests/HostedServices/TwitterBotHostedServiceTests.cs
+++ GrammarNazi.Tests/HostedServices/TwitterBotHostedServiceTests.cs
@@ -1,12 +1,17 @@
 ﻿using GrammarNazi.App.HostedServices;
+using GrammarNazi.Domain.Entities;
 using GrammarNazi.Domain.Entities.Configs;
 using GrammarNazi.Domain.Enums;
 using GrammarNazi.Domain.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 using Xunit;
 
 namespace GrammarNazi.Tests.HostedServices
@@ -33,7 +38,44 @@ namespace GrammarNazi.Tests.HostedServices
             await hostedService.StartAsync(cancellationTokenSource.Token);
 
             // Assert
-            // If any exception thrown, test pass
+            // If no exception thrown, test pass
+        }
+
+        [Fact]
+        public async Task NoLastTweetId_Should_UsePageSize()
+        {
+            // Assert
+            var twitterClientMock = new Mock<ITwitterClient>();
+            var twitterLogServiceMock = new Mock<ITwitterLogService>();
+            var scheduleTweetServiceMock = new Mock<IScheduledTweetService>();
+            var twitterSettingsOptionsMock = new Mock<IOptions<TwitterBotSettings>>();
+            var grammarServiceMock = new Mock<IGrammarService>();
+            var cancellationTokenSource = new CancellationTokenSource();
+            var twitterSettings = new TwitterBotSettings { BotUsername = "botUser", TimelineFirstLoadPageSize = 15 };
+
+            twitterSettingsOptionsMock.Setup(v => v.Value).Returns(twitterSettings);
+            twitterLogServiceMock.Setup(v => v.GetLastTweetId()).ReturnsAsync(0);
+            twitterClientMock.Setup(v => v.Users.GetFollowerIdsAsync(twitterSettings.BotUsername))
+                .ReturnsAsync(new long[] { 1 });
+            twitterClientMock.Setup(v => v.Timelines.GetUserTimelineAsync(It.IsAny<GetUserTimelineParameters>()))
+                .ReturnsAsync(new ITweet[0]);
+            twitterClientMock.Setup(v => v.Users.GetFriendIdsAsync(twitterSettings.BotUsername))
+                .ReturnsAsync(new long[0]);
+            scheduleTweetServiceMock.Setup(v => v.GetPendingScheduledTweets())
+                .ReturnsAsync(Enumerable.Empty<ScheduledTweet>());
+            grammarServiceMock.Setup(v => v.GrammarAlgorith).Returns(GrammarAlgorithms.LanguageToolApi);
+
+            var hostedService = new TwitterBotHostedService(Mock.Of<ILogger<TwitterBotHostedService>>(), new[] { grammarServiceMock.Object }, twitterLogServiceMock.Object, twitterClientMock.Object, twitterSettingsOptionsMock.Object, null, scheduleTweetServiceMock.Object);
+
+            // Act
+            var startTask = new Thread(async () => await hostedService.StartAsync(cancellationTokenSource.Token));
+            startTask.Start(); // Act in new Thread to avoid blocking current thread
+            await Task.Delay(150);
+            cancellationTokenSource.Cancel();
+            startTask.Join();
+
+            // Assert
+            twitterClientMock.Verify(v => v.Timelines.GetUserTimelineAsync(It.Is<GetUserTimelineParameters>(g => g.PageSize == twitterSettings.TimelineFirstLoadPageSize)));
         }
     }
 }
