05048608de59d0bb8a637a708a084e92a8c65f8b
nminaya <nelsonminayaperez@gmail.com>
2021-01-08 22:24:01 -0400

    Add TweetWithRtPrefix_Should_Not_AnalyzeTweet test

--- GrammarNazi.Tests/HostedServices/TwitterBotHostedServiceTests.cs
+++ GrammarNazi.Tests/HostedServices/TwitterBotHostedServiceTests.cs
@@ -115,5 +115,47 @@ namespace GrammarNazi.Tests.HostedServices
             // Assert
             twitterClientMock.Verify(v => v.Timelines.GetUserTimelineAsync(It.Is<GetUserTimelineParameters>(g => g.SinceId == lastTweetId)));
         }
+
+        [Fact]
+        public async Task TweetWithRtPrefix_Should_Not_AnalyzeTweet()
+        {
+            // Assert
+            var twitterClientMock = new Mock<ITwitterClient>();
+            var twitterLogServiceMock = new Mock<ITwitterLogService>();
+            var scheduleTweetServiceMock = new Mock<IScheduledTweetService>();
+            var twitterSettingsOptionsMock = new Mock<IOptions<TwitterBotSettings>>();
+            var grammarServiceMock = new Mock<IGrammarService>();
+            var tweetMock = new Mock<ITweet>();
+            var cancellationTokenSource = new CancellationTokenSource();
+            var twitterSettings = new TwitterBotSettings { BotUsername = "botUser", TimelineFirstLoadPageSize = 15 };
+            const long lastTweetId = 123456;
+
+            tweetMock.Setup(v => v.Text).Returns("RT @TwitterUser This is a tweet");
+
+            twitterSettingsOptionsMock.Setup(v => v.Value).Returns(twitterSettings);
+            twitterLogServiceMock.Setup(v => v.GetLastTweetId()).ReturnsAsync(lastTweetId);
+            twitterClientMock.Setup(v => v.Users.GetFollowerIdsAsync(twitterSettings.BotUsername))
+                .ReturnsAsync(new long[] { 1 });
+            twitterClientMock.Setup(v => v.Timelines.GetUserTimelineAsync(It.IsAny<GetUserTimelineParameters>()))
+                .ReturnsAsync(new[] { tweetMock.Object });
+            twitterClientMock.Setup(v => v.Users.GetFriendIdsAsync(twitterSettings.BotUsername))
+                .ReturnsAsync(new long[0]);
+            scheduleTweetServiceMock.Setup(v => v.GetPendingScheduledTweets())
+                .ReturnsAsync(Enumerable.Empty<ScheduledTweet>());
+            grammarServiceMock.Setup(v => v.GrammarAlgorith).Returns(GrammarAlgorithms.LanguageToolApi);
+
+            var hostedService = new TwitterBotHostedService(Mock.Of<ILogger<TwitterBotHostedService>>(), new[] { grammarServiceMock.Object }, twitterLogServiceMock.Object, twitterClientMock.Object, twitterSettingsOptionsMock.Object, null, scheduleTweetServiceMock.Object);
+
+            // Act
+            var startTask = new Thread(async () => await hostedService.StartAsync(cancellationTokenSource.Token));
+            startTask.Start(); // Act in new Thread to avoid blocking current thread
+            await Task.Delay(150);
+            cancellationTokenSource.Cancel();
+            startTask.Join();
+
+            // Assert
+            grammarServiceMock.Verify(v => v.GetCorrections(It.IsAny<string>()), Times.Never);
+            twitterLogServiceMock.Verify(v => v.LogTweet(It.IsAny<long>()), Times.Never);
+        }
     }
 }
