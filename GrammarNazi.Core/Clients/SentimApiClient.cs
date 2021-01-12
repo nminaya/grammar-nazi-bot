745855761f6daeba48506db95fb52e0d0ce16ec5
nminaya <nelsonminayaperez@gmail.com>
2021-01-11 22:04:31 -0400

    Add ISentimApiClient

---
+++ GrammarNazi.Core/Clients/SentimApiClient.cs
@@ -0,0 +1,32 @@
+﻿using GrammarNazi.Domain.Clients;
+using GrammarNazi.Domain.Entities.SentimAPI;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Core.Clients
+{
+    public class SentimApiClient : ISentimApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SentimApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<SentimResult> GetSentimentResult(string text)
+        {
+            // TODO: Get url from config
+            const string url = "https://sentim-api.herokuapp.com/api/v1/";
+
+            var body = JsonContent.Create(new SentimRequest(text));
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.PostAsync(url, body);
+
+            return JsonConvert.DeserializeObject<SentimResult>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ GrammarNazi.Domain/Clients/ISentimApiClient.cs
@@ -0,0 +1,10 @@
+﻿using GrammarNazi.Domain.Entities.SentimAPI;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Domain.Clients
+{
+    public interface ISentimApiClient
+    {
+        Task<SentimResult> GetSentimentResult(string text);
+    }
+}
