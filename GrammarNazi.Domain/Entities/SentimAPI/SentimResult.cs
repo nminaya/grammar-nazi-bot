59d58aee7a93f3115e03fe482c15e8138bbf822a
nminaya <nelsonminayaperez@gmail.com>
2021-01-11 22:04:13 -0400

    Add Sentim API entities

---
+++ GrammarNazi.Domain/Entities/SentimAPI/SentimResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace GrammarNazi.Domain.Entities.SentimAPI
+{
+    public class SentimResult
+    {
+        [JsonPropertyName("result")]
+        public SentimentCheckResult Result { get; set; }
+
+        [JsonPropertyName("sentences")]
+        public List<SentimSentence> Sentences { get; set; }
+    }
+}
---
+++ GrammarNazi.Domain/Entities/SentimAPI/SentimentCheckResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GrammarNazi.Domain.Entities.SentimAPI
+{
+    public class SentimentCheckResult
+    {
+        [JsonPropertyName("polarity")]
+        public double Polarity { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
