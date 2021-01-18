fbfc4379c433bb18d2934dadc31d6f4656163080
nminaya <nelsonminayaperez@gmail.com>
2021-01-18 08:06:09 -0400

    Add IDiscordChannelConfigService

---
+++ GrammarNazi.Domain/Services/IDiscordChannelConfigService.cs
@@ -0,0 +1,16 @@
+﻿using GrammarNazi.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Domain.Services
+{
+    public interface IDiscordChannelConfigService
+    {
+        Task AddConfiguration(DiscordChannelConfig channelConfig);
+
+        Task Delete(DiscordChannelConfig channelConfig);
+
+        Task Update(DiscordChannelConfig channelConfig);
+
+        Task<DiscordChannelConfig> GetConfigurationByChannelId(ulong channelId);
+    }
+}
