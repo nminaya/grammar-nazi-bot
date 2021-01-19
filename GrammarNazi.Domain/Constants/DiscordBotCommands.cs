0f217f24d8c0305fe3496d9075e5b84701bfcb9b
nminaya <nelsonminayaperez@gmail.com>
2021-01-18 21:42:03 -0400

    Create DiscordBotCommands.cs

---
+++ GrammarNazi.Domain/Constants/DiscordBotCommands.cs
@@ -0,0 +1,21 @@
+﻿namespace GrammarNazi.Domain.Constants
+{
+    public static class DiscordBotCommands
+    {
+        public const string Prefix = "!";
+
+        public const string Start = "start";
+        public const string Help = "help";
+        public const string Settings = "settings";
+        public const string SetAlgorithm = "set_algorithm";
+        public const string Language = "lang";
+        public const string Stop = "stop";
+        public const string HideDetails = "hide_details";
+        public const string ShowDetails = "show_details";
+        public const string Tolerant = "tolerant";
+        public const string Intolerant = "intolerant";
+        public const string WhiteList = "whitelist";
+        public const string AddWhiteList = "add_whitelist";
+        public const string RemoveWhiteList = "remove_whitelist";
+    }
+}
