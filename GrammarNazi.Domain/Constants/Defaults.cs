743e5ee234b00b0fbfeb1fa6af7c4dfa24a01140
nminaya <nelsonminayaperez@gmail.com>
2021-01-26 21:26:43 -0400

    Update Valid positive sentiment score

--- GrammarNazi.Domain/Constants/Defaults.cs
+++ GrammarNazi.Domain/Constants/Defaults.cs
@@ -9,7 +9,7 @@ namespace GrammarNazi.Domain.Constants
         public const int StringComparableRange = 2;
         public const GrammarAlgorithms DefaultAlgorithm = GrammarAlgorithms.LanguageToolApi;
         public const string TelegramBotUser = "grammarNz_Bot";
-        public const double ValidPositiveSentimentScore = 0.90;
+        public const double ValidPositiveSentimentScore = 0.60;
         public const int GithubIssueMaxTitleLength = 256;
         public const int TwitterTextMaxLength = 280;
     }
