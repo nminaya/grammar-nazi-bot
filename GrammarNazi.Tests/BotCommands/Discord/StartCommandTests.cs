ea6e0dcd525bca70ea7812db26c5d3a7c410be17
nminaya <nelsonminayaperez@gmail.com>
2021-03-01 19:27:13 -0400

    Add BotStoppedAndUserNotAdmin_Should_ReplyNotAdminMessage test

--- GrammarNazi.Tests/BotCommands/Discord/StartCommandTests.cs
+++ GrammarNazi.Tests/BotCommands/Discord/StartCommandTests.cs
@@ -43,5 +43,38 @@ namespace GrammarNazi.Tests.BotCommands.Discord
             // Verify SendMessageAsync was called with the reply message "Bot is already started"
             channelMock.Verify(v => v.SendMessageAsync(null, false, It.Is<Embed>(e => e.Description.Contains(replyMessage)), null, null, null));
         }
+
+        [Fact]
+        public async Task BotStoppedAndUserNotAdmin_Should_ReplyNotAdminMessage()
+        {
+            // Arrange
+            var chatConfigurationServiceMock = new Mock<IDiscordChannelConfigService>();
+            var command = new StartCommand(chatConfigurationServiceMock.Object);
+            const string replyMessage = "Only admins can use this command";
+
+            var chatConfig = new DiscordChannelConfig
+            {
+                IsBotStopped = true
+            };
+
+            var channelMock = new Mock<IMessageChannel>();
+            var user = new Mock<IGuildUser>();
+            user.Setup(v => v.GuildPermissions).Returns(GuildPermissions.None);
+            var message = new Mock<IMessage>();
+
+            message.Setup(v => v.Author).Returns(user.Object);
+            message.Setup(v => v.Channel).Returns(channelMock.Object);
+
+            chatConfigurationServiceMock.Setup(v => v.GetConfigurationByChannelId(message.Object.Channel.Id))
+                .ReturnsAsync(chatConfig);
+
+            // Act
+            await command.Handle(message.Object);
+
+            // Assert
+
+            // Verify SendMessageAsync was called with the reply message "Only admins can use this command"
+            channelMock.Verify(v => v.SendMessageAsync(null, false, It.Is<Embed>(e => e.Description.Contains(replyMessage)), null, null, null));
+        }
     }
 }
