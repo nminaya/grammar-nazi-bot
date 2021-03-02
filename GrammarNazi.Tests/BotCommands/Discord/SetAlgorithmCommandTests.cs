4e2713321cbcf0c1ee5243f9572ad1d933e21256
nminaya <nelsonminayaperez@gmail.com>
2021-03-02 08:31:57 -0400

    Update SetAlgorithmCommandTests.cs

--- GrammarNazi.Tests/BotCommands/Discord/SetAlgorithmCommandTests.cs
+++ GrammarNazi.Tests/BotCommands/Discord/SetAlgorithmCommandTests.cs
@@ -84,6 +84,40 @@ namespace GrammarNazi.Tests.BotCommands.Discord
             Assert.Equal(GrammarAlgorithms.InternalAlgorithm, chatConfig.GrammarAlgorithm); // Make sure SelectedLanguage is still Auto
         }
 
+        [Fact]
+        public async Task ParameterNotReceived_Should_ReplyMessage()
+        {
+            // Arrange
+            var channelConfigurationServiceMock = new Mock<IDiscordChannelConfigService>();
+            var command = new SetAlgorithmCommand(channelConfigurationServiceMock.Object);
+            const string replyMessage = "Parameter not received";
+
+            var chatConfig = new DiscordChannelConfig
+            {
+                GrammarAlgorithm = GrammarAlgorithms.InternalAlgorithm
+            };
+
+            var channelMock = new Mock<IMessageChannel>();
+            var user = new Mock<IGuildUser>();
+            user.Setup(v => v.GuildPermissions).Returns(GuildPermissions.All);
+            var message = new Mock<IMessage>();
+            message.Setup(v => v.Content).Returns(DiscordBotCommands.SetAlgorithm);
+            message.Setup(v => v.Author).Returns(user.Object);
+            message.Setup(v => v.Channel).Returns(channelMock.Object);
+
+            channelConfigurationServiceMock.Setup(v => v.GetConfigurationByChannelId(message.Object.Channel.Id))
+                .ReturnsAsync(chatConfig);
+
+            // Act
+            await command.Handle(message.Object);
+
+            // Assert
+
+            // Verify SendMessageAsync was called with the reply message "Parameter not received"
+            channelMock.Verify(v => v.SendMessageAsync(null, false, It.Is<Embed>(e => e.Description.Contains(replyMessage)), null, null, null));
+            Assert.Equal(GrammarAlgorithms.InternalAlgorithm, chatConfig.GrammarAlgorithm); // Make sure SelectedLanguage is still Auto
+        }
+
         [Theory]
         [InlineData(GrammarAlgorithms.InternalAlgorithm)]
         [InlineData(GrammarAlgorithms.DatamuseApi)]
