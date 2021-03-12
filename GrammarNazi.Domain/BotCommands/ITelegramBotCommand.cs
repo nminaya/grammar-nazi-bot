4937871f68949375e4168b085cb1bc9dc4f1c278
Jadhiel Vélez <jadhielv@gmail.com>
2021-03-12 17:05:43 -0400

    Create ITelegramBotCommand.cs

---
+++ GrammarNazi.Domain/BotCommands/ITelegramBotCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace GrammarNazi.Domain.BotCommands
+{
+    public interface ITelegramBotCommand
+    {
+        string Command { get; }
+        Task Handle(Message message);
+    }
+}
