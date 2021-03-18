e0c5a2a2c793033a5c42597ecdcee2ca0e8bbc33
nminaya <nelsonminayaperez@gmail.com>
2021-03-18 09:12:28 -0400

    Update RemoveWhiteListCommand.cs

--- GrammarNazi.Core/BotCommands/Discord/RemoveWhiteListCommand.cs
+++ GrammarNazi.Core/BotCommands/Discord/RemoveWhiteListCommand.cs
@@ -1,8 +1,9 @@
 ﻿using Discord;
-using Discord.WebSocket;
+using GrammarNazi.Core.Utilities;
 using GrammarNazi.Domain.BotCommands;
 using GrammarNazi.Domain.Constants;
 using GrammarNazi.Domain.Services;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace GrammarNazi.Core.BotCommands.Discord
@@ -38,7 +39,7 @@ namespace GrammarNazi.Core.BotCommands.Discord
 
             var word = parameters[1].Trim();
 
-            if (!channelConfig.WhiteListWords.Contains(word))
+            if (!channelConfig.WhiteListWords.Contains(word, new CaseInsensitiveEqualityComparer()))
             {
                 await SendMessage(message, $"The word '{word}' is not in the WhiteList.", DiscordBotCommands.RemoveWhiteList);
                 return;
