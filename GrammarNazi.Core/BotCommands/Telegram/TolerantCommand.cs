0470f5ca9fee4e2db48822650b2b0be759d54b90
Jadhiel Vélez <jadhielv@gmail.com>
2021-03-18 17:54:04 -0400

    Create TolerantCommand

---
+++ GrammarNazi.Core/BotCommands/Telegram/TolerantCommand.cs
@@ -0,0 +1,45 @@
+﻿using GrammarNazi.Domain.BotCommands;
+using GrammarNazi.Domain.Constants;
+using GrammarNazi.Domain.Enums;
+using GrammarNazi.Domain.Services;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace GrammarNazi.Core.BotCommands.Telegram
+{
+    public class TolerantCommand : BaseTelegramCommand, ITelegramBotCommand
+    {
+        private readonly IChatConfigurationService _chatConfigurationService;
+        private readonly ITelegramBotClient _client;
+
+        public string Command => TelegramBotCommands.Tolerant;
+
+        public TolerantCommand(IChatConfigurationService chatConfigurationService,
+            ITelegramBotClient telegramBotClient)
+            : base(telegramBotClient)
+        {
+            _chatConfigurationService = chatConfigurationService;
+            _client = telegramBotClient;
+        }
+
+        public async Task Handle(Message message)
+        {
+            if (!await IsUserAdmin(message))
+            {
+                await _client.SendTextMessageAsync(message.Chat.Id, "Only admins can use this command.", replyToMessageId: message.MessageId);
+                return;
+            }
+
+            var chatConfig = await _chatConfigurationService.GetConfigurationByChatId(message.Chat.Id);
+
+            chatConfig.CorrectionStrictnessLevel = CorrectionStrictnessLevels.Tolerant;
+
+            await _chatConfigurationService.Update(chatConfig);
+
+            await _client.SendTextMessageAsync(message.Chat.Id, "Tolerant ✅");
+
+            await NotifyIfBotIsNotAdmin(message);
+        }
+    }
+}
