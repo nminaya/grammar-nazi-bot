ca9462995ca3752edb284b39b244f2c8160a0be2
nminaya <nelsonminayaperez@gmail.com>
2021-03-18 08:57:09 -0400

    Create CaseInsensitiveComparer.cs

---
+++ GrammarNazi.Core/Utilities/CaseInsensitiveComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GrammarNazi.Core.Utilities
+{
+    public class CaseInsensitiveComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            if (x == null && y == null)
+                return true;
+
+            return x?.ToLower() == y?.ToLower();
+        }
+
+        public int GetHashCode([DisallowNull] string obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
