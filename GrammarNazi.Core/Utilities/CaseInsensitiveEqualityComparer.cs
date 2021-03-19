b2e5ed95a287baac857690d948bd86284c5160fc
nminaya <nelsonminayaperez@gmail.com>
2021-03-18 21:08:03 -0400

    Update CaseInsensitiveEqualityComparer.cs

--- GrammarNazi.Core/Utilities/CaseInsensitiveEqualityComparer.cs
+++ GrammarNazi.Core/Utilities/CaseInsensitiveEqualityComparer.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace GrammarNazi.Core.Utilities
 {
-    public class CaseInsensitiveEqualityComparer : IEqualityComparer<string>
+    internal class CaseInsensitiveEqualityComparer : IEqualityComparer<string>
     {
         public bool Equals(string x, string y)
         {
