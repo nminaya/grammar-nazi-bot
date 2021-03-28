3e55061760ad5b7a4b69a7d342475f71113c9996
Jadhiel Vélez <jadhielv@gmail.com>
2021-03-28 18:39:58 -0400

    Create AddWhiteListCommandTests.cs

--- GrammarNazi.Tests/TestUtilities.cs
+++ GrammarNazi.Tests/TestUtilities.cs
@@ -2,6 +2,9 @@
 using GrammarNazi.Domain.BotCommands;
 using Moq;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace GrammarNazi.Tests
 {
@@ -27,6 +30,32 @@ namespace GrammarNazi.Tests
 
             // Verify SendMessageAsync was called with the reply message "Only admins can use this command"
             channelMock.Verify(v => v.SendMessageAsync(It.Is<string>(s => s.Contains(replyMessage)), false, null, null, null, It.Is<MessageReference>(m => m.MessageId.Value == message.Object.Id)));
+        }
+
+        public static async Task TestTelegramNotAdminUser(ITelegramBotCommand command, Mock<ITelegramBotClient> telegramBotClientMock) 
+        {
+            // Arrange
+            const string replyMessage = "Only admins can use this command.";
+
+            var message = new Message
+            {
+                Text = command.Command,
+                From = new User { Id = 2 },
+                Chat = new Chat
+                {
+                    Id = 1,
+                    Type = ChatType.Group
+                }
+            };
+
+            telegramBotClientMock.Setup(v => v.GetChatAdministratorsAsync(It.IsAny<ChatId>(), default))
+                .ReturnsAsync(new ChatMember[0]);
+
+            // Act
+            await command.Handle(message);
+
+            // Assert
+            telegramBotClientMock.Verify(v => v.SendTextMessageAsync(It.IsAny<ChatId>(), It.Is<string>(s => s.Contains(replyMessage)), ParseMode.Default, false, false, 0, null, default));
         }
     }
 }
