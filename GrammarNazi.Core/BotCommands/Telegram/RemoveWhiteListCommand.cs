d5437e6f5dcf8142119d5c2d75b3a39a8cf3313c
nminaya <nelsonminayaperez@gmail.com>
2021-03-29 19:50:03 -0400

    Case sensitive validation for RemoveWhiteListCommand

--- GrammarNazi.Core/BotCommands/Telegram/RemoveWhiteListCommand.cs
+++ GrammarNazi.Core/BotCommands/Telegram/RemoveWhiteListCommand.cs
@@ -1,6 +1,9 @@
-﻿using GrammarNazi.Domain.BotCommands;
+﻿using GrammarNazi.Core.Utilities;
+using GrammarNazi.Domain.BotCommands;
 using GrammarNazi.Domain.Constants;
 using GrammarNazi.Domain.Services;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -42,13 +45,13 @@ namespace GrammarNazi.Core.BotCommands.Telegram
 
                 var word = parameters[1].Trim();
 
-                if (!chatConfig.WhiteListWords.Contains(word))
+                if (!chatConfig.WhiteListWords.Contains(word, new CaseInsensitiveEqualityComparer()))
                 {
                     await Client.SendTextMessageAsync(message.Chat.Id, $"The word '{word}' is not in the WhiteList.");
                     return;
                 }
 
-                chatConfig.WhiteListWords.Remove(word);
+                chatConfig.WhiteListWords.RemoveAll(v => v.Equals(word, StringComparison.OrdinalIgnoreCase));
 
                 await _chatConfigurationService.Update(chatConfig);
 
