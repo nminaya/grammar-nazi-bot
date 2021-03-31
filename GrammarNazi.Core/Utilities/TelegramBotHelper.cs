658f1193cb0f2856b88ed71a15fbe7064e6f2f20
nminaya <nelsonminayaperez@gmail.com>
2021-03-30 22:16:30 -0400

    Create TelegramBotHelper.cs

---
+++ GrammarNazi.Core/Utilities/TelegramBotHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace GrammarNazi.Core.Utilities
+{
+    internal static class TelegramBotHelper
+    {
+        public static async Task<bool> IsUserAdmin(ITelegramBotClient client, Message message, User user = null)
+        {
+            if (message.Chat.Type == ChatType.Private)
+                return true;
+
+            var chatAdministrators = await client.GetChatAdministratorsAsync(message.Chat.Id);
+            var currentUserId = user?.Id ?? message.From.Id;
+
+            return chatAdministrators.Any(v => v.User.Id == currentUserId);
+        }
+    }
+}
