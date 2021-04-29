f4e99a37a08b2c9c114aac4ae1fd5bcafdca8727
Nelson Minaya <nelsonminayaperez@gmail.com>
2021-04-29 19:32:22 -0400

    Make SentimentAnalysisResult a struct

--- GrammarNazi.Domain/Entities/SentimentAnalysisResult.cs
+++ GrammarNazi.Domain/Entities/SentimentAnalysisResult.cs
@@ -2,7 +2,7 @@
 
 namespace GrammarNazi.Domain.Entities
 {
-    public class SentimentAnalysisResult
+    public struct SentimentAnalysisResult
     {
         public SentimentTypes Type { get; init; }
         public double Score { get; init; }
