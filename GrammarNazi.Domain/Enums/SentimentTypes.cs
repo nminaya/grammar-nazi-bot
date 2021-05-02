6fa3e2161892d1349621b1d6b9acf8a086fd356a
Nelson Minaya <nelsonminayaperez@gmail.com>
2021-05-02 19:10:38 -0400

    Make Neutral default

--- GrammarNazi.Domain/Enums/SentimentTypes.cs
+++ GrammarNazi.Domain/Enums/SentimentTypes.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GrammarNazi.Domain.Enums
+﻿namespace GrammarNazi.Domain.Enums
 {
     public enum SentimentTypes
     {
-        Negative,
         Neutral,
+        Negative,
         Positive
     }
 }
