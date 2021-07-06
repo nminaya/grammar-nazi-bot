404c250707a9aa2059eeaf7c9da5a862d493c53c
Nelson Minaya <nelsonminayaperez@gmail.com>
2021-07-05 22:12:00 -0400

    Create HandleUpdate_MessageReceivedNotTextType_Should_DoNothing

--- GrammarNazi.Tests/Utilities/TelegramUpdateHandlerTests.cs
+++ GrammarNazi.Tests/Utilities/TelegramUpdateHandlerTests.cs
@@ -1,11 +1,12 @@
 ﻿using GrammarNazi.Core.Utilities;
+using GrammarNazi.Domain.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 using Xunit;
 
 namespace GrammarNazi.Tests.Utilities
@@ -38,6 +39,38 @@ namespace GrammarNazi.Tests.Utilities
                             It.IsAny<It.IsAnyType>(),
                             It.IsAny<Exception>(),
                             (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()));
+        }
+
+        [Fact]
+        public async Task HandleUpdate_MessageReceivedNotTextType_Should_DoNothing()
+        {
+            // Arrange
+            var telegramBotMock = new Mock<ITelegramBotClient>();
+            var chatConfigServiceMock = new Mock<IChatConfigurationService>();
+            var serviceScopeFactory = new Mock<IServiceScopeFactory>();
+            var serviceScope = new Mock<IServiceScope>();
+            var serviceProvider = new Mock<IServiceProvider>();
+
+            var update = new Update
+            {
+                Message = new Message
+                {
+                    Audio = new Audio(),
+                    Chat = new Chat { Id = 1 }
+                }
+            };
+
+            serviceScopeFactory.Setup(x => x.CreateScope()).Returns(serviceScope.Object);
+            serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProvider.Object);
+            serviceProvider.Setup(x => x.GetService(typeof(IChatConfigurationService))).Returns(chatConfigServiceMock.Object);
+
+            var handler = new TelegramUpdateHandler(serviceScopeFactory.Object, null, null);
+
+            // Act
+            await handler.HandleUpdate(telegramBotMock.Object, update, default);
+
+            // Assert
+            chatConfigServiceMock.Verify(x => x.GetConfigurationByChatId(update.Message.Chat.Id), Times.Never);
         }
     }
 }
