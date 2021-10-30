b016a08d72a5c5db85810bb3c1234c810ddcd12c
Nelson <nelsonminayaperez@gmail.com>
2021-10-30 18:09:45 -0400

    Add new unit test

--- GrammarNazi.Tests/Utilities/TelegramUpdateHandlerTests.cs
+++ GrammarNazi.Tests/Utilities/TelegramUpdateHandlerTests.cs
@@ -1,10 +1,12 @@
 ﻿using GrammarNazi.Core.Utilities;
+using GrammarNazi.Domain.Entities;
 using GrammarNazi.Domain.Enums;
 using GrammarNazi.Domain.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
@@ -73,6 +75,48 @@ namespace GrammarNazi.Tests.Utilities
 
             // Assert
             chatConfigServiceMock.Verify(x => x.GetConfigurationByChatId(update.Message.Chat.Id), Times.Never);
+        }
+
+        [Fact]
+        public async Task HandleUpdate_MessageReceived_Should_GetCorrections()
+        {
+            // Arrange
+            var telegramBotMock = new Mock<ITelegramBotClient>();
+            var chatConfigServiceMock = new Mock<IChatConfigurationService>();
+            var serviceScopeFactory = new Mock<IServiceScopeFactory>();
+            var serviceScope = new Mock<IServiceScope>();
+            var serviceProvider = new Mock<IServiceProvider>();
+            var loggerMock = new Mock<ILogger<TelegramUpdateHandler>>();
+            var grammarService = new Mock<IGrammarService>();
+
+            var update = new Update
+            {
+                Message = new Message
+                {
+                    Text = "My Text",
+                    Chat = new Chat { Id = 1 }
+                }
+            };
+
+            chatConfigServiceMock.Setup(x => x.GetConfigurationByChatId(update.Message.Chat.Id))
+                .ReturnsAsync(new ChatConfiguration());
+
+            grammarService.Setup(x => x.GetCorrections("My Text"))
+                .ReturnsAsync(new GrammarCheckResult(null));
+
+            serviceScopeFactory.Setup(x => x.CreateScope()).Returns(serviceScope.Object);
+            serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProvider.Object);
+            serviceProvider.Setup(x => x.GetService(typeof(IChatConfigurationService))).Returns(chatConfigServiceMock.Object);
+            serviceProvider.Setup(x => x.GetService(typeof(IEnumerable<IGrammarService>))).Returns(new[] { grammarService.Object });
+
+            var handler = new TelegramUpdateHandler(serviceScopeFactory.Object, null, loggerMock.Object);
+
+            // Act
+            await handler.HandleUpdate(telegramBotMock.Object, update, default);
+
+            // Assert
+            chatConfigServiceMock.Verify(x => x.GetConfigurationByChatId(update.Message.Chat.Id));
+            grammarService.Verify(x => x.GetCorrections("My Text"));
         }
 
         [Theory]
