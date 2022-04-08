9fc4cd66a05c099ea5308e63ea1fb31ef3ed62f3
Nelson <nelsonminayaperez@gmail.com>
2022-04-07 20:52:39 -0400

    Use File Scoped Namespaces in GrammarNazi.App project

--- GrammarNazi.App/HostedServices/Twitter/TwitterBotMentionHostedService.cs
+++ GrammarNazi.App/HostedServices/Twitter/TwitterBotMentionHostedService.cs
@@ -18,156 +18,155 @@ using Tweetinvi.Exceptions;
 using Tweetinvi.Models;
 using Tweetinvi.Parameters;
 
-namespace GrammarNazi.App.HostedServices
+namespace GrammarNazi.App.HostedServices;
+
+public class TwitterBotMentionHostedService : BaseTwitterHostedService
 {
-    public class TwitterBotMentionHostedService : BaseTwitterHostedService
+    private readonly IGrammarService _grammarService;
+    private readonly IGithubService _githubService;
+    private readonly ITwitterMentionLogService _twitterMentionLogService;
+
+    public TwitterBotMentionHostedService(ILogger<TwitterBotHostedService> logger,
+        IEnumerable<IGrammarService> grammarServices,
+        ITwitterLogService twitterLogService,
+        ITwitterClient twitterClient,
+        IOptions<TwitterBotSettings> options,
+        IGithubService githubService,
+        IScheduledTweetService scheduledTweetService,
+        ITwitterMentionLogService twitterMentionLogService,
+        ISentimentAnalysisService sentimentAnalysisService)
+        : base(logger, twitterLogService, twitterClient, options.Value, scheduledTweetService, sentimentAnalysisService)
     {
-        private readonly IGrammarService _grammarService;
-        private readonly IGithubService _githubService;
-        private readonly ITwitterMentionLogService _twitterMentionLogService;
+        _githubService = githubService;
+        _twitterMentionLogService = twitterMentionLogService;
+        _grammarService = grammarServices.First(v => v.GrammarAlgorith == Defaults.DefaultAlgorithm);
+        _grammarService.SetStrictnessLevel(CorrectionStrictnessLevels.Intolerant);
+    }
 
-        public TwitterBotMentionHostedService(ILogger<TwitterBotHostedService> logger,
-            IEnumerable<IGrammarService> grammarServices,
-            ITwitterLogService twitterLogService,
-            ITwitterClient twitterClient,
-            IOptions<TwitterBotSettings> options,
-            IGithubService githubService,
-            IScheduledTweetService scheduledTweetService,
-            ITwitterMentionLogService twitterMentionLogService,
-            ISentimentAnalysisService sentimentAnalysisService)
-            : base(logger, twitterLogService, twitterClient, options.Value, scheduledTweetService, sentimentAnalysisService)
-        {
-            _githubService = githubService;
-            _twitterMentionLogService = twitterMentionLogService;
-            _grammarService = grammarServices.First(v => v.GrammarAlgorith == Defaults.DefaultAlgorithm);
-            _grammarService.SetStrictnessLevel(CorrectionStrictnessLevels.Intolerant);
-        }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Logger.LogInformation("TwitterBotMentionHostedService started");
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            Logger.LogInformation("TwitterBotMentionHostedService started");
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                try
-                {
-                    long lastTweetId = await _twitterMentionLogService.GetLastTweetId();
-
-                    var getMentionParameters = new GetMentionsTimelineParameters();
-
-                    if (lastTweetId == 0)
-                        getMentionParameters.PageSize = TwitterBotSettings.TimelineFirstLoadPageSize;
-                    else
-                        getMentionParameters.SinceId = lastTweetId;
-
-                    var mentions = await TwitterClient.Timelines.GetMentionsTimelineAsync(getMentionParameters);
-
-                    var myUser = await TwitterClient.Users.GetUserAsync(TwitterBotSettings.BotUsername);
-
-                    foreach (var mention in mentions.Where(x => x.Text.Contains($"@{TwitterBotSettings.BotUsername}") && (x.InReplyToStatusId.HasValue || x.QuotedStatusId.HasValue)))
-                    {
-                        // Avoid correcting replies to my own tweets
-                        if (mention.InReplyToUserId == myUser.Id)
-                            continue;
-
-                        var tweet = await GetTweetFromMention(mention);
-
-                        if (tweet == null)
-                            continue;
-
-                        var tweetText = StringUtils.RemoveHashtags(StringUtils.RemoveMentions(StringUtils.RemoveEmojis(tweet.Text)));
-
-                        var correctionsResult = await _grammarService.GetCorrections(tweetText);
-
-                        if (!correctionsResult.HasCorrections)
-                        {
-                            await PublishReplyTweet($"@{mention.CreatedBy.ScreenName} I don't see anything wrong here.", mention.Id);
-                            continue;
-                        }
-
-                        var messageBuilder = new StringBuilder();
-
-                        messageBuilder.Append($"@{mention.CreatedBy.ScreenName}");
-
-                        foreach (var correction in correctionsResult.Corrections)
-                        {
-                            // Only suggest the first possible replacement
-                            messageBuilder.AppendLine($"*{correction.PossibleReplacements.First()} [{correction.Message}]");
-                        }
-
-                        var correctionString = messageBuilder.ToString();
-
-                        Logger.LogInformation($"Sending reply to: {mention.CreatedBy.ScreenName}");
-
-                        if (correctionString.Length >= Defaults.TwitterTextMaxLength)
-                        {
-                            var replyTweets = correctionString.SplitInParts(Defaults.TwitterTextMaxLength);
-
-                            foreach (var (reply, index) in replyTweets.WithIndex())
-                            {
-                                var correctionStringSplitted = index == 0 ? reply : $"@{mention.CreatedBy.ScreenName} {reply}";
-
-                                await PublishReplyTweet(correctionStringSplitted, mention.Id);
-
-                                await Task.Delay(TwitterBotSettings.PublishTweetDelayMilliseconds, stoppingToken);
-                            }
-
-                            continue;
-                        }
-
-                        await PublishReplyTweet(correctionString, mention.Id);
-
-                        await Task.Delay(TwitterBotSettings.PublishTweetDelayMilliseconds, stoppingToken);
-                    }
-
-                    if (mentions.Any())
-                    {
-                        var lastTweet = mentions.OrderByDescending(v => v.Id).First();
-
-                        // Save last Tweet Id
-                        await _twitterMentionLogService.LogTweet(lastTweet.Id, default);
-                    }
-
-                    var followersTask = TwitterClient.Users.GetFollowersAsync(TwitterBotSettings.BotUsername);
-                    var friendIdsTask = TwitterClient.Users.GetFriendIdsAsync(TwitterBotSettings.BotUsername);
-
-                    await FollowBackUsers(await followersTask, await friendIdsTask);
-                    await PublishScheduledTweets();
-                    await LikeRepliesToBot(mentions);
-                }
-                catch (TwitterException ex) when (ex.TwitterDescription.Contains("Try again later") || ex.TwitterDescription.Contains("Timeout limit"))
-                {
-                    Logger.LogWarning(ex, ex.TwitterDescription);
-                }
-                catch (SqlException ex) when (ex.Message.Contains("SHUTDOWN"))
-                {
-                    Logger.LogWarning(ex, "Sql Server shutdown in progress");
-                }
-                catch (Exception ex)
-                {
-                    var message = ex is TwitterException tEx ? tEx.TwitterDescription : ex.Message;
-
-                    Logger.LogError(ex, message);
-
-                    // fire and forget
-                    _ = _githubService.CreateBugIssue($"Application Exception: {message}", ex, GithubIssueLabels.Twitter);
-                }
-
-                await Task.Delay(TwitterBotSettings.HostedServiceIntervalMilliseconds);
-            }
-        }
-
-        private async Task<ITweet> GetTweetFromMention(ITweet mention)
+        while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
-                return await TwitterClient.Tweets.GetTweetAsync(mention.InReplyToStatusId ?? mention.QuotedStatusId.Value);
+                long lastTweetId = await _twitterMentionLogService.GetLastTweetId();
+
+                var getMentionParameters = new GetMentionsTimelineParameters();
+
+                if (lastTweetId == 0)
+                    getMentionParameters.PageSize = TwitterBotSettings.TimelineFirstLoadPageSize;
+                else
+                    getMentionParameters.SinceId = lastTweetId;
+
+                var mentions = await TwitterClient.Timelines.GetMentionsTimelineAsync(getMentionParameters);
+
+                var myUser = await TwitterClient.Users.GetUserAsync(TwitterBotSettings.BotUsername);
+
+                foreach (var mention in mentions.Where(x => x.Text.Contains($"@{TwitterBotSettings.BotUsername}") && (x.InReplyToStatusId.HasValue || x.QuotedStatusId.HasValue)))
+                {
+                    // Avoid correcting replies to my own tweets
+                    if (mention.InReplyToUserId == myUser.Id)
+                        continue;
+
+                    var tweet = await GetTweetFromMention(mention);
+
+                    if (tweet == null)
+                        continue;
+
+                    var tweetText = StringUtils.RemoveHashtags(StringUtils.RemoveMentions(StringUtils.RemoveEmojis(tweet.Text)));
+
+                    var correctionsResult = await _grammarService.GetCorrections(tweetText);
+
+                    if (!correctionsResult.HasCorrections)
+                    {
+                        await PublishReplyTweet($"@{mention.CreatedBy.ScreenName} I don't see anything wrong here.", mention.Id);
+                        continue;
+                    }
+
+                    var messageBuilder = new StringBuilder();
+
+                    messageBuilder.Append($"@{mention.CreatedBy.ScreenName}");
+
+                    foreach (var correction in correctionsResult.Corrections)
+                    {
+                        // Only suggest the first possible replacement
+                        messageBuilder.AppendLine($"*{correction.PossibleReplacements.First()} [{correction.Message}]");
+                    }
+
+                    var correctionString = messageBuilder.ToString();
+
+                    Logger.LogInformation($"Sending reply to: {mention.CreatedBy.ScreenName}");
+
+                    if (correctionString.Length >= Defaults.TwitterTextMaxLength)
+                    {
+                        var replyTweets = correctionString.SplitInParts(Defaults.TwitterTextMaxLength);
+
+                        foreach (var (reply, index) in replyTweets.WithIndex())
+                        {
+                            var correctionStringSplitted = index == 0 ? reply : $"@{mention.CreatedBy.ScreenName} {reply}";
+
+                            await PublishReplyTweet(correctionStringSplitted, mention.Id);
+
+                            await Task.Delay(TwitterBotSettings.PublishTweetDelayMilliseconds, stoppingToken);
+                        }
+
+                        continue;
+                    }
+
+                    await PublishReplyTweet(correctionString, mention.Id);
+
+                    await Task.Delay(TwitterBotSettings.PublishTweetDelayMilliseconds, stoppingToken);
+                }
+
+                if (mentions.Any())
+                {
+                    var lastTweet = mentions.OrderByDescending(v => v.Id).First();
+
+                    // Save last Tweet Id
+                    await _twitterMentionLogService.LogTweet(lastTweet.Id, default);
+                }
+
+                var followersTask = TwitterClient.Users.GetFollowersAsync(TwitterBotSettings.BotUsername);
+                var friendIdsTask = TwitterClient.Users.GetFriendIdsAsync(TwitterBotSettings.BotUsername);
+
+                await FollowBackUsers(await followersTask, await friendIdsTask);
+                await PublishScheduledTweets();
+                await LikeRepliesToBot(mentions);
             }
-            catch (TwitterException ex) when (ex.ToString().Contains("blocked from the author of this tweet"))
+            catch (TwitterException ex) when (ex.TwitterDescription.Contains("Try again later") || ex.TwitterDescription.Contains("Timeout limit"))
             {
-                Logger.LogWarning(ex, $"Blocked from {mention.CreatedBy.ScreenName}");
-                return null;
+                Logger.LogWarning(ex, ex.TwitterDescription);
             }
+            catch (SqlException ex) when (ex.Message.Contains("SHUTDOWN"))
+            {
+                Logger.LogWarning(ex, "Sql Server shutdown in progress");
+            }
+            catch (Exception ex)
+            {
+                var message = ex is TwitterException tEx ? tEx.TwitterDescription : ex.Message;
+
+                Logger.LogError(ex, message);
+
+                // fire and forget
+                _ = _githubService.CreateBugIssue($"Application Exception: {message}", ex, GithubIssueLabels.Twitter);
+            }
+
+            await Task.Delay(TwitterBotSettings.HostedServiceIntervalMilliseconds);
+        }
+    }
+
+    private async Task<ITweet> GetTweetFromMention(ITweet mention)
+    {
+        try
+        {
+            return await TwitterClient.Tweets.GetTweetAsync(mention.InReplyToStatusId ?? mention.QuotedStatusId.Value);
+        }
+        catch (TwitterException ex) when (ex.ToString().Contains("blocked from the author of this tweet"))
+        {
+            Logger.LogWarning(ex, $"Blocked from {mention.CreatedBy.ScreenName}");
+            return null;
         }
     }
 }
