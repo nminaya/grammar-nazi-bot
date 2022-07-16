ee82e13b9c01462b69b0ce9ae2b6a2dbbfb67b3d
Nelson <nelsonminayaperez@gmail.com>
2022-07-15 21:18:31 -0400

    Finalize CatchExceptionService

--- GrammarNazi.Core/Services/CatchExceptionService.cs
+++ GrammarNazi.Core/Services/CatchExceptionService.cs
@@ -1,7 +1,9 @@
-﻿using GrammarNazi.Core.Extensions;
+﻿using Discord.Net;
+using GrammarNazi.Core.Extensions;
 using GrammarNazi.Domain.Enums;
 using GrammarNazi.Domain.Exceptions;
 using GrammarNazi.Domain.Services;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
@@ -9,6 +11,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
 using Telegram.Bot.Exceptions;
+using Tweetinvi.Exceptions;
 
 namespace GrammarNazi.Core.Services
 {
@@ -16,6 +19,12 @@ namespace GrammarNazi.Core.Services
     {
         private readonly ILogger<CatchExceptionService> _logger;
         private readonly IGithubService _githubService;
+
+        public CatchExceptionService(IGithubService githubService, ILogger<CatchExceptionService> logger)
+        {
+            _githubService = githubService;
+            _logger = logger;
+        }
 
         public void HandleException(Exception exception, GithubIssueLabels githubIssueSection)
         {
@@ -34,10 +43,56 @@ namespace GrammarNazi.Core.Services
                     HandleHttpRequestException(httpRequestException, githubIssueSection);
                     break;
 
+                case SqlException sqlException:
+                    HandleSqlException(sqlException, githubIssueSection);
+                    break;
+
+                case HttpException httpException:
+                    HandleHttpException(httpException, githubIssueSection);
+                    break;
+
+                case TwitterException twitterException:
+                    HandleTwitterException(twitterException, githubIssueSection);
+                    break;
+
                 default:
                     HandleGeneralException(exception, githubIssueSection);
                     break;
             }
+        }
+
+        private void HandleTwitterException(TwitterException twitterException, GithubIssueLabels githubIssueSection)
+        {
+            if (twitterException.TwitterDescription.Contains("Try again later") || twitterException.TwitterDescription.Contains("Timeout limit"))
+            {
+                _logger.LogWarning(twitterException, twitterException.TwitterDescription);
+                return;
+            }
+
+            HandleGeneralException(twitterException, githubIssueSection);
+        }
+
+        private void HandleHttpException(HttpException httpException, GithubIssueLabels githubIssueSection)
+        {
+            if (httpException.Message.ContainsAny("50013", "50001", "Forbidden", "160002") 
+                || httpException.HttpCode == HttpStatusCode.BadRequest)
+            {
+                _logger.LogWarning(httpException, httpException.Message);
+                return;
+            }
+
+            HandleGeneralException(httpException, githubIssueSection);
+        }
+
+        private void HandleSqlException(SqlException sqlException, GithubIssueLabels githubIssueSection)
+        {
+            if (sqlException.Message.Contains("SHUTDOWN"))
+            {
+                _logger.LogWarning(sqlException, "Sql Server shutdown in progress");
+                return;
+            }
+
+            HandleGeneralException(sqlException, githubIssueSection);
         }
 
         private void HandleHttpRequestException(HttpRequestException requestException, GithubIssueLabels githubIssueSection)
@@ -58,15 +113,16 @@ namespace GrammarNazi.Core.Services
             if (warningMessages.Any(x => apiRequestException.Message.Contains(x)))
             {
                 _logger.LogWarning(apiRequestException.Message);
+                return;
             }
-            else
-            {
-                _logger.LogError(apiRequestException, apiRequestException.Message);
-            }
+         
+            _logger.LogError(apiRequestException, apiRequestException.Message);
         }
 
         private void HandleGeneralException(Exception exception, GithubIssueLabels githubIssueSection)
         {
+            var message = exception is TwitterException tEx ? tEx.TwitterDescription : exception.Message;
+
             var innerExceptions = exception.GetInnerExceptions();
 
             if (innerExceptions.Any(x => x.GetType() == typeof(SocketException) && x.Message.Contains("Connection reset by peer")))
@@ -77,10 +133,10 @@ namespace GrammarNazi.Core.Services
                 return;
             }
 
-            _logger.LogError(exception, exception.Message);
+            _logger.LogError(exception, message);
 
             // fire and forget
-            _ = _githubService.CreateBugIssue($"Application Exception: {exception.Message}", exception, githubIssueSection);
+            _ = _githubService.CreateBugIssue($"Application Exception: {message}", exception, githubIssueSection);
         }
     }
 }
