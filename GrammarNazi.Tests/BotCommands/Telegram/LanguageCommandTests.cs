1f8cbd13f9d40ad6f84157c79e14f2a9ae9ee610
Nelson <nelsonminayaperez@gmail.com>
2022-10-30 10:24:45 -0400

    Update LanguageCommandTests.cs

--- GrammarNazi.Tests/BotCommands/Telegram/LanguageCommandTests.cs
+++ GrammarNazi.Tests/BotCommands/Telegram/LanguageCommandTests.cs
@@ -1,4 +1,5 @@
 ﻿using GrammarNazi.Core.BotCommands.Telegram;
+using GrammarNazi.Core.Extensions;
 using GrammarNazi.Domain.Constants;
 using GrammarNazi.Domain.Entities;
 using GrammarNazi.Domain.Enums;
@@ -143,6 +144,52 @@ public class LanguageCommandTests
         // Assert
         Assert.Equal(languageParameter, chatConfig.SelectedLanguage);
         telegramBotClientMock.Verify(v => v.SendTextMessageAsync(message.Chat.Id, It.Is<string>(s => s.Contains(replyMessage)), default, default, default, default, default, default, default, default, default));
+    }
+
+    [Fact]
+    public async Task LanguageNotSupportedByCurrentAlgorithm_Should_SendWarningMessage()
+    {
+        // Arrange
+        var chatConfigurationServiceMock = new Mock<IChatConfigurationService>();
+        var telegramBotClientMock = new Mock<ITelegramBotClientWrapper>();
+        var command = new LanguageCommand(chatConfigurationServiceMock.Object, telegramBotClientMock.Object);
+
+        var chatConfig = new ChatConfiguration
+        {
+            SelectedLanguage = SupportedLanguages.English,
+            GrammarAlgorithm = GrammarAlgorithms.YandexSpellerApi
+        };
+
+        var message = new Message
+        {
+            Text = $"{TelegramBotCommands.Language} {(int)SupportedLanguages.French}",
+            From = new User { Id = 2 },
+            Chat = new Chat
+            {
+                Id = 1,
+                Type = ChatType.Group
+            }
+        };
+
+        telegramBotClientMock.Setup(v => v.GetChatAdministratorsAsync(message.Chat.Id, default))
+            .ReturnsAsync(new[] { new ChatMemberMember { User = new() { Id = message.From.Id } } });
+
+        telegramBotClientMock.Setup(v => v.GetMeAsync(default))
+            .ReturnsAsync(new User { Id = 123456 });
+
+        chatConfigurationServiceMock.Setup(v => v.GetConfigurationByChatId(message.Chat.Id))
+            .ReturnsAsync(chatConfig);
+
+        // Act
+        await command.Handle(message);
+
+        // Assert
+        Assert.Equal(SupportedLanguages.French, chatConfig.SelectedLanguage);
+        telegramBotClientMock.Verify(v => v.SendTextMessageAsync(message.Chat.Id, It.Is<string>(s => s.Contains("Language updated")), default, default, default, default, default, default, default, default, default));
+
+        var warningMessage = $"WARNING: The selected language ({SupportedLanguages.French.GetDescription()}) is not supported by the selected algorithm ({GrammarAlgorithms.YandexSpellerApi.GetDescription()}).";
+
+        telegramBotClientMock.Verify(v => v.SendTextMessageAsync(message.Chat.Id, It.Is<string>(s => s.Contains(warningMessage)), default, default, default, default, default, default, default, default, default));
     }
 
     [Fact]
