d15ddc714b973218b34ef99c399f767d3e354eda
nminaya <nelsonminayaperez@gmail.com>
2025-02-18 20:32:33 -0400

    Start implementation of client

---
+++ GrammarNazi.Core/Clients/GeminiApiClient.cs
@@ -0,0 +1,36 @@
+﻿using GrammarNazi.Domain.Entities.GeminiAPI;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace GrammarNazi.Core.Clients;
+
+public class GeminiApiClient(IHttpClientFactory httpClientFactory)
+{
+    public async Task<GenerateContentResponse> GenerateContent(string promt)
+    {
+        // TODO: Get API from config
+        var gemeniApiKey = "";
+
+        var httpClient = httpClientFactory.CreateClient("geminiApi");
+
+        var request = new HttpRequestMessage(HttpMethod.Post, $"v1beta/models/gemini-1.5-flash:generateContent?key={gemeniApiKey}")
+        {
+            Content = JsonContent.Create(GenerateContentRequest.CreateRequestObject(promt))
+        };
+
+        var response = await httpClient.SendAsync(request);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new InvalidOperationException($"Unsuccessful Gemini API response {response.StatusCode}", new(await response.Content.ReadAsStringAsync()));
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return JsonConvert.DeserializeObject<GenerateContentResponse>(content);
+    }
+}
---
+++ GrammarNazi.Domain/Entities/GeminiAPI/GenerateContentResponse.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace GrammarNazi.Domain.Entities.GeminiAPI;
+
+public class GenerateContentResponse
+{
+    [JsonProperty("candidates")]
+    public List<Candidate> Candidates { get; set; }
+
+    [JsonProperty("usageMetadata")]
+    public UsageMetadata UsageMetadata { get; set; }
+
+    [JsonProperty("modelVersion")]
+    public string ModelVersion { get; set; }
+}
+
+public class Candidate
+{
+    [JsonProperty("content")]
+    public ContentResponse Content { get; set; }
+
+    [JsonProperty("finishReason")]
+    public string FinishReason { get; set; }
+
+    [JsonProperty("avgLogprobs")]
+    public decimal AvgLogprobs { get; set; }
+}
+
+public class CandidatesTokensDetail
+{
+    [JsonProperty("modality")]
+    public string Modality { get; set; }
+
+    [JsonProperty("tokenCount")]
+    public int TokenCount { get; set; }
+}
+
+public class ContentResponse
+{
+    [JsonProperty("parts")]
+    public List<Part> Parts { get; set; }
+
+    [JsonProperty("role")]
+    public string Role { get; set; }
+}
+
+public class PromptTokensDetail
+{
+    [JsonProperty("modality")]
+    public string Modality { get; set; }
+
+    [JsonProperty("tokenCount")]
+    public int TokenCount { get; set; }
+}
+
+public class UsageMetadata
+{
+    [JsonProperty("promptTokenCount")]
+    public int PromptTokenCount { get; set; }
+
+    [JsonProperty("candidatesTokenCount")]
+    public int CandidatesTokenCount { get; set; }
+
+    [JsonProperty("totalTokenCount")]
+    public int TotalTokenCount { get; set; }
+
+    [JsonProperty("promptTokensDetails")]
+    public List<PromptTokensDetail> PromptTokensDetails { get; set; }
+
+    [JsonProperty("candidatesTokensDetails")]
+    public List<CandidatesTokensDetail> CandidatesTokensDetails { get; set; }
+}
